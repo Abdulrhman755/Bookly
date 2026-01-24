7f0d4374598f98fe3acdf37c322ed1112a94cd16
Abdulrhman755 <strikelord945@gmail.com>
2026-01-24 17:12:40 +0200

    transition

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 const kPrimaryColor = Color(0xff100B20);
-const ktransitionDuration = Duration(milliseconds: 500);
+const ktransitionDuration = Duration(milliseconds: 750);
 const kPoppinsFontFamily = 'Poppins';
 const kFeaturedBox = 'featured_box';
 const kNewestBox = 'newest_box';
